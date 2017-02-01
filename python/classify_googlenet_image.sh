24597f0f053b6e6c1e5f73e8b47c45520f17a8c5
AOYAMA Kazuharu <aoyama@ideeinc.co.jp>
2017-02-01 22:00:33 +0900

    add tools.

---
+++ python/classify_googlenet_image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+RESULT=result-$$.npy
+
+trap 'rm -f $RESULT' 2 3 15 EXIT
+
+[ -z "$1" ] && exit
+cd `dirname $0`
+python ./classify.py --gpu  --model_def ../models/bvlc_googlenet/deploy.prototxt --pretrained_model ../models/bvlc_googlenet/bvlc_googlenet.caffemodel --images_dim 224,224 --mean_file googlenet_mean.npy  $@ $RESULT 2>/dev/null
+echo
+python ./show_npy.py ../data/ilsvrc12/synset_words.txt $RESULT
