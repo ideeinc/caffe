53b8b2cf66c88e00705e54c8d7bd996db21dea3c
AOYAMA Kazuharu <aoyama@ideeinc.co.jp>
2017-02-01 22:00:33 +0900

    add tools.

---
+++ python/classify_image.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+RESULT=result-$$.npy
+
+trap 'rm -f $RESULT' 2 3 15 EXIT
+
+[ -z "$1" ] && exit
+cd `dirname $0`
+python ./classify.py --gpu $@ $RESULT 2>/dev/null
+echo
+python ./show_npy.py ../data/ilsvrc12/synset_words.txt $RESULT
