cabe97b491eca00a49c3e10deb6dcddb21fcbdc8
AOYAMA Kazuharu <aoyama@ideeinc.co.jp>
2017-06-02 16:02:20 +0900

    update detect.sh

--- scripts/detect.sh
+++ scripts/detect.sh
@@ -1,20 +1,50 @@
-#!/bin/bash -x
+#!/bin/bash
 #
 # ./detect.sh model-root-path image-file
 #
-FILELISTTXT=.flist
-DETECTLIST=.detected
+absolute_path() {
+  if [ -d "$1" ]; then
+    cd "$1"
+    pwd
+  else
+    echo $1
+  fi
+}
+
+absolute_filepath() {
+  if [ -f "$1" ]; then
+    DIR=$(absolute_path `dirname $1`)
+    echo $DIR/`basename $1`
+  else
+    echo $1
+  fi
+}
+
+#
+FILELISTTXT=/tmp/.flist$$
+DETECTLIST=/tmp/.detected$$
+JPGFILE=`absolute_filepath $2`
 RESIMG=`basename $2`.png
-THRESHOLD=0.2
-MODELROOT=$1
-cd $CAFFE_ROOT
+THRESHOLD=0.4
+MODELROOT=`absolute_path $1`
 
 trap 'rm -f $FILELISTTXT $DETECTLIST $RESIMG' 2 3 15 EXIT
 
-[ -f "$2" -a -n "$MODELROOT" ] || exit
+if [ ! -f "$JPGFILE" -o ! -n "$MODELROOT" ]; then
+  echo "no such file or directory."
+  exit
+fi
 CAFFEMODEL=`ls $MODELROOT/*.caffemodel | sort | tail -1`
-echo $2 > $FILELISTTXT
-./build/examples/ssd/ssd_detect --confidence_threshold=$THRESHOLD $MODELROOT/deploy.prototxt $CAFFEMODEL $FILELISTTXT | tee $DETECTLIST
+echo $JPGFILE > $FILELISTTXT
+
+cd $CAFFE_ROOT
+./build/examples/ssd/ssd_detect --confidence_threshold=$THRESHOLD $MODELROOT/deploy.prototxt $CAFFEMODEL $FILELISTTXT 2>/dev/null | tee $DETECTLIST
+if [ -z "`cat $DETECTLIST`" ]; then
+  echo 'no detection'
+  eog $JPGFILE
+  exit
+fi
 
 python ./examples/ssd/plot_detections.py $DETECTLIST / --save-dir .
 [ -f $RESIMG ] && eog $RESIMG
+
