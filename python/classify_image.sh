82d52352092405c448993f3d63f56af5f8f47927
AOYAMA Kazuharu <aoyama@ideeinc.co.jp>
2017-10-18 20:54:49 +0900

    update script

--- python/classify_image.sh
+++ python/classify_image.sh
@@ -6,6 +6,6 @@ trap 'rm -f $RESULT' 2 3 15 EXIT
 
 [ -z "$1" ] && exit
 cd `dirname $0`
-python ./classify.py --gpu $@ $RESULT 2>/dev/null
+python ./classify.py --gpu --output $RESULT $@ 2>/dev/null
 echo
 python ./show_npy.py ../data/ilsvrc12/synset_words.txt $RESULT
